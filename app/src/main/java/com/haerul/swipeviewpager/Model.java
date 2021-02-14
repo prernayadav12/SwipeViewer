03596e6fed79649c64c9f77c82804da6366448ad
prernayadav12 <prernay8112@gmail.com>
2021-02-14 16:40:20 +0530

    Done with the designs

---
+++ app/src/main/java/com/haerul/swipeviewpager/Model.java
@@ -0,0 +1,29 @@
+package com.haerul.swipeviewpager;
+
+public class Model {
+
+    private int image;
+    private String title;
+    private String desc;
+
+    public Model(int image, String title, String desc) {
+        this.image = image;
+        this.title = title;
+        this.desc = desc;
+    }
+
+    public int getImage() {
+        return image;
+    }
+
+
+    public String getTitle() {
+        return title;
+    }
+
+
+    public String getDesc() {
+        return desc;
+    }
+
+}
